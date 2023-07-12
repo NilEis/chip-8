e2de504038bfd5c2b1bc00175a79db8f76e9f1e0
NilEis <52820950+NilEis@users.noreply.github.com>
2023-07-13 00:25:11 +0200

    Initial commit

---
+++ include/defines.h
@@ -0,0 +1,13 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <stdint.h>
+
+#define CHIP_MEM_SIZE 0x1000
+
+#define CHIP_DISPLAY_WIDTH 64
+#define CHIP_DISPLAY_HEIGHT 32
+
+typedef uint8_t byte_t;
+
+#endif // DEFINES_H
